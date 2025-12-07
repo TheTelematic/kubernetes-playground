e403d7c14784d3a9dd89a7ab5aa1979b8bf1434f
Arturo Parra <arturoteleco95@protonmail.ch>
2025-12-07 16:41:56 +0100

    wip

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,7 +1,7 @@
 import time
 
 from gpiozero import LED
-import kubernetes
+from kubernetes import config, client
 
 white_led = LED(2)
 blue_led = LED(3)
@@ -11,8 +11,18 @@ yellow_led = LED(27)
 red_led = LED(22)
 
 
-def main():
-    white_led.on()
+config.load_kube_config(config_file="/etc/rancher/k3s/k3s.yaml")
+
+
+def list_pods():
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+def blink_leds():
     leds = [
         blue_led,
         green_led_1,
@@ -25,8 +35,14 @@ def main():
         for led in leds:
             previous_led.off()
             led.on()
-            time.sleep(1)
+            list_pods()
             previous_led = led
+
+
+def main():
+    white_led.on()
+    blink_leds()
+
 
 if __name__ == '__main__':
     try:
