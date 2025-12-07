fbbb6afa2e1265e58834ea68f521da7be0b2ac59
Arturo Parra <arturoteleco95@protonmail.ch>
2025-12-07 17:44:02 +0100

    wip

---
+++ locust/fibonacci.py
@@ -0,0 +1,10 @@
+import random
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def fibonacci(self):
+        self.client.get("/fibonacci", params={"n": random.randint(0, 100)})
