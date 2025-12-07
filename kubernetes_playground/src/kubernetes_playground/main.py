4e448462aa34067d4e40b4af6ee1ab0dfbadbb8b
Arturo Parra <arturoteleco95@protonmail.ch>
2025-12-07 17:38:42 +0100

    wip

--- kubernetes_playground/src/kubernetes_playground/main.py
+++ kubernetes_playground/src/kubernetes_playground/main.py
@@ -1,6 +1,18 @@
+import asyncio
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+async def get_fibonacci(n: int) -> int:
+    await asyncio.sleep(0)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return await get_fibonacci(n - 1) + await get_fibonacci(n - 2)
 
 
 @app.get("/liveness")
@@ -11,3 +23,8 @@ async def liveness():
 @app.get("/readiness")
 async def readiness():
     return "ok"
+
+
+@app.get("/fibonacci")
+async def fibonacci(n: int):
+    return await get_fibonacci(n)
